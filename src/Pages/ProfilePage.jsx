09642e9c043b1f23f21573ed8fc034f20e29ab0f
My <myglimsjo@gmail.com>
2025-09-01 10:17:37 +0200

    U

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -202,13 +202,13 @@ function ProfilePage() {
                   </strong>
                 </p>
                 <p>
-                  Primär: {valueProfiles?.[item?.primaryType]?.title ?? item?.primaryType ?? "Okänd"} ({item?.primaryMatchPercentage ?? "–"}%)
+                  Primär: {valueProfiles?.[item?.primaryType]?.name ?? item?.primaryType ?? "Okänd"} ({item?.primaryMatchPercentage ?? "–"}%)
                 </p>
                 <p>
-                  Sekundär: {valueProfiles?.[item?.secondaryType]?.title ?? item?.secondaryType ?? "Okänd"} ({item?.secondaryMatchPercentage ?? "–"}%)
+                  Sekundär: {valueProfiles?.[item?.secondaryType]?.name ?? item?.secondaryType ?? "Okänd"} ({item?.secondaryMatchPercentage ?? "–"}%)
                 </p>
                 <p>
-                  Tredje: {valueProfiles?.[item?.thirdType]?.title ?? item?.thirdType ?? "Okänd"} ({item?.thirdMatchPercentage ?? "–"}%)
+                  Tredje: {valueProfiles?.[item?.thirdType]?.name ?? item?.thirdType ?? "Okänd"} ({item?.thirdMatchPercentage ?? "–"}%)
                 </p>
                 <hr />
               </div>
