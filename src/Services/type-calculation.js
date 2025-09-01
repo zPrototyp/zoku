eb543000992dfd57ef5c854d3454a034acf0980e
Jenny Evertsson <jentap@gmail.com>
2025-09-01 14:43:15 +0200

    make the calculation graceful

--- src/Services/type-calculation.js
+++ src/Services/type-calculation.js
@@ -6,6 +6,16 @@ export function calculatePersonalityType(changeVsTradition, compassionVsAmbition
 // Helper to calculate match percentage between two personalities
 export function calculateMatchPercentage(personalityOne, personalityTwo)
 {
+   if (
+    !personalityOne || 
+    !personalityTwo ||
+    typeof personalityOne.changeVsTradition !== "number" ||
+    typeof personalityOne.compassionVsAmbition !== "number" ||
+    typeof personalityTwo.changeVsTradition !== "number" ||
+    typeof personalityTwo.compassionVsAmbition !== "number"
+  ) {
+    return 0;
+  }
     var distance = Math.sqrt(
         Math.pow(personalityOne.changeVsTradition - personalityTwo.changeVsTradition, 2) +
         Math.pow(personalityOne.compassionVsAmbition - personalityTwo.compassionVsAmbition, 2)
