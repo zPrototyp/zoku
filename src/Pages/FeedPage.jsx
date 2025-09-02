dfdba890a3bd4e16601c7f7e8f6db0a39e33a634
Jenny Evertsson <43755330+Lia-De@users.noreply.github.com>
2025-09-02 10:39:44 +0200

    Update FeedPage.jsx
    fix

--- src/Pages/FeedPage.jsx
+++ src/Pages/FeedPage.jsx
@@ -145,7 +145,7 @@ export default function FeedPage() {
             </select>
           </div>
 
-          {sortedFeed.length ==0 && 
+          {sortedFeed && sortedFeed.length ==0 && 
           <div className="feed">Inga varumärken att visa just nu</div>
           }
           {sortedFeed && sortedFeed.length > 0 (
